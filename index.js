d874b7b23dbc26d3dc145fea37bee2c3861a140f
nikolaisid <nikolaisid@gmail.com>
2016-12-16 01:01:34 +1100

    json error

--- index.js
+++ index.js
@@ -3,29 +3,37 @@ var app = express();
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
+app.use(function(error, request, response, next) {
+	if (error instanceof SyntaxError) {
+		response.status(400).json( {
+			error: 'Could not decode request: JSON parsing failed'
+		}).send();
+	}
+});
 
-app.post('/', function(request, response) {	
-	var data = JSON.parse(request.body, function(err) {
-		if (err) {
-			response.status(400).json( {
-				error: 'Could not decode request: JSON parsing failed'
-			}).send();
-		}
-		else {
-			var response = new Array();
-			for (var i = 0; i < data['take']; i++) {
-				var show = data['payload'][i];
-				if (show['drm'] == true && show['episodeCount'] > 0) {
-					response.push({
-						'image': show['image']['showImage'],
-						'slug': show['slug'],
-						'title': show['title']
-					});
-				}
+app.post('/', function(request, response) {
+		var data = request.body;
+		var response = new Array();
+		for (var i = 0; i < data['take']; i++) {
+			var show = data['payload'][i];
+			if (show['drm'] == true && show['episodeCount'] > 0) {
+				response.push({
+					'image': show['image']['showImage'],
+					'slug': show['slug'],
+					'title': show['title']
+				});
 			}
-			response.send(JSON.stringify({'response': response}));
 		}
-	});
+		response.send(JSON.stringify({'response': response}));
 });
 
 app.listen(process.env.PORT || 5000);
+
+function IsValidJSON(data) {
+	try {
+		JSON.parse(data);
+	} catch (error) {
+		return false;
+	}
+	return true;
+}
